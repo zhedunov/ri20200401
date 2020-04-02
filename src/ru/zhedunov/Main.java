621e8235601f105d5d4f56df6527cb693f6a4942
jake_r7@mail.ru <Leopard7_>
2020-04-02 13:58:54 +0400

    Коммит из общаги-2

--- src/ru/zhedunov/Main.java
+++ src/ru/zhedunov/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello, world!");
         System.out.println("Hello, world-2!");
+        System.out.println("Hello, world-3!");
     }
 }
